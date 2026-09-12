0c6391e47bd4f6198d00d82643f3e5174f22a3d6
David Ruiz Requejo <davidrr.22.12@gmail.com>
2026-09-12 17:24:51 +0200

    fix/media Correct media mobile menu, and hero

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -14,4 +14,14 @@ import {
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  menuOpen = false;
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
+}
