4badc1a8f9db7392a9988d7a8fe5c2ab1b600c26
David Ruiz Requejo <davidrr.22.12@gmail.com>
2026-09-12 12:19:03 +0200

    fix/artwork-publication Add images and pdf, and correct footer redirection

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,3 +1,14 @@
 import { Component, Input } from '@angular/core';
-@Component({selector:'app-footer',standalone:true,templateUrl:'./footer.component.html',styleUrl:'./footer.component.scss'})
-export class FooterComponent { @Input() instagram='https://www.instagram.com/vicenteruizdeza/'; @Input() email='vruiz77@gmail.com'; }
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  @Input() instagram = 'https://www.instagram.com/vicenteruizdeza/';
+  @Input() email = 'vruiz77@gmail.com';
+}
